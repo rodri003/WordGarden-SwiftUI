c38f2b656309fe97ef681e527f3fccb5a1e10c32
Rafael Rodriguez <rafael.nc@me.com>
2024-05-14 15:28:09 -0400

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Rafael Rodriguez on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
